0b718970e43b9a034a27442723233411a51f88c6
Matthew Duffield <matt.duffield@gmail.com>
2024-11-22 10:57:28 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Go Bookings
+This is a bookings web application using Go.
+
+
+
+## Tips
+
+To compile multiple files in Go:
+```bash
+go run .
+```
+
+After refactoring:
+```bash
+go run cmd/web/*.go
+```
+
+Last structure:
+```bash
+go run ./cmd/web
+```
+
+
+
---
+++ cmd/web/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/mattduffield/go_bookings/pkg/config"
+	"github.com/mattduffield/go_bookings/pkg/handlers"
+	"github.com/mattduffield/go_bookings/pkg/render"
+)
+
+const port string = ":8090"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the entry point
+func main() {
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	fmt.Printf("Starting application on port %s\n", port)
+
+	srv := &http.Server{
+		Addr:    port,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
